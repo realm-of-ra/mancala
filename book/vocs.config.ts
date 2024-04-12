7f9f9b6cff94b35e0383883758ad3c087a0f4028
Tony Stark <oktonystark8@gmail.com>
2024-04-12 06:05:49 -0500

    issue: for review on client

--- book/vocs.config.ts
+++ book/vocs.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vocs'
+import { defineConfig } from "vocs";
 
 export default defineConfig({
   socials: [
-      {
-        icon: 'github',
-        link: 'https://github.com/realm-of-ra/mancala',
-      },
-      {
-        icon: 'x',
-        link: 'https://twitter.com/starknetafrica',
-      },
-    ],
-  title: 'Mancala',
-  sidebar: [
     {
-      text: 'Overview',
-      link: '/getting-started',
+      icon: "github",
+      link: "https://github.com/realm-of-ra/mancala",
     },
     {
-      text: 'Game Play',
-      link: '/game-play',
+      icon: "x",
+      link: "https://twitter.com/realm_of_ra",
     },
   ],
-})
+  title: "Mancala",
+  sidebar: [
+    {
+      text: "Overview",
+      link: "/getting-started",
+    },
+    {
+      text: "Game Play",
+      link: "/game-play",
+    },
+  ],
+});
