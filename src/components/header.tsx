2144b289ec55ed677fe7232194c234c76f3251bb
supreme2580 <victoromorogbe69@gmail.com>
2024-06-06 18:10:39 +0100

    join game

---
+++ src/components/header.tsx
@@ -0,0 +1,136 @@
+
+import { useEffect, useRef } from "react";
+import { truncateString } from "../lib/utils";
+import mancala from "../assets/logo.png";
+import eniola from "../assets/eniola.png";
+import muteImage from "../assets/mute.png";
+import unmuteImage from "../assets/unmute.png";
+import { connect, disconnect } from 'starknetkit';
+import { useAtom } from "jotai";
+import { isPlayingAtom, profileDataAtom, addressAtom, connectionAtom } from "../atom/atoms";
+import audio from "../music/audio_1.mp4";
+import { useProvider } from "@starknet-react/core";
+import { StarknetIdNavigator } from "starknetid.js";
+import { constants } from "starknet";
+import { Button } from "@material-tailwind/react";
+import { UserIcon } from "@heroicons/react/24/solid";
+import { StarkProfile } from "@/types";
+
+export default function Header() {
+    const [connection, setConnection] = useAtom(connectionAtom);
+    const [address, setAddress] = useAtom(addressAtom);
+    const [profileData, setProfileData] = useAtom<StarkProfile>(profileDataAtom)
+
+    const { provider } = useProvider();
+    const starknetIdNavigator = new StarknetIdNavigator(
+        provider,
+        constants.StarknetChainId.SN_MAIN
+    );
+
+    const connectWallet = async () => {
+        if (connection?.isConnected) {
+            disconnectWallet();
+        }
+        else {
+            const { wallet } = await connect({ modalMode: "canAsk" })
+            if (wallet && wallet.isConnected) {
+                setConnection(wallet);
+                setAddress(wallet.selectedAddress);
+                const starkProfile = await starknetIdNavigator.getProfileData(wallet.selectedAddress);
+                setProfileData(starkProfile)
+            }
+        }
+    }
+    const disconnectWallet = async () => {
+        await disconnect();
+        setConnection(undefined);
+        setAddress('');
+        setProfileData({});
+    }
+    const [isPlaying, setPlaying] = useAtom(isPlayingAtom);
+    const audioRef = useRef(new Audio(audio));
+    useEffect(() => {
+        if (isPlaying) {
+            try {
+                audioRef.current.play();
+                audioRef.current.loop = true;
+            } catch (error) {
+                console.error("Error playing the audio", error);
+            }
+        } else {
+            audioRef.current.pause();
+        }
+        if (!connection?.isConnected) {
+            connectWallet();
+        }
+        return () => {
+            audioRef.current.pause();
+        };
+    }, [isPlaying]);
+    const togglePlay = () => {
+        setPlaying(!isPlaying);
+    }
+    return (
+        <div className="flex flex-row items-center justify-between w-full">
+            <div className="flex-1 w-full -mr-10">
+                {
+                    profileData.name != undefined ? (
+                        <div className="flex flex-row space-x-2.5 items-center justify-end">
+                            <div className="p-1 rounded-full bg-gradient-to-r bg-[#15181E] from-[#2E323A] via-[#4B505C] to-[#1D2026] relative">
+                                <img src={profileData.profilePicture ? profileData.profilePicture : eniola} width={60} height={60} alt="Eniola" className="rounded-full" />
+                                <div className="absolute bottom-0 right-0 h-6 w-6 bg-[#15171E] rounded-full flex flex-col items-center justify-center">
+                                    <div className="h-4 w-4 bg-[#00FF57] rounded-full" />
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className="text-2xl text-right text-white">{profileData.name ? profileData.name : truncateString(address)}</h3>
+                                <h4 className="text-sm text-[#F58229] text-start">Level 6</h4>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="flex flex-row space-x-2.5 items-center justify-end">
+                            <div className="p-1 rounded-full bg-gradient-to-r bg-[#15181E] from-[#2E323A] via-[#4B505C] to-[#1D2026] relative">
+                                <div className="bg-[#15171E] rounded-full p-2.5">
+                                    <UserIcon color="#F58229" className="w-8 h-8" />
+                                    <div className="absolute bottom-0 right-0 h-6 w-6 bg-[#15171E] rounded-full flex flex-col items-center justify-center">
+                                        <div className="h-4 w-4 bg-[#00FF57] rounded-full" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className="text-2xl text-right text-white">Player</h3>
+                                <h4 className="text-sm text-[#F58229] text-start">Guest</h4>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+            <div className="h-[100px] w-[800px]">
+                <div className="bg-[url('./assets/leaderboard-top.png')] w-[800px] h-[100px] bg-contain bg-no-repeat flex flex-col items-center justify-center">
+                    <a href="/" className="mb-4">
+                        <img src={mancala} className="h-10 w-36" />
+                    </a>
+                </div>
+            </div>
+            <div className="flex-1 w-full -ml-16">
+                <div className="flex flex-row space-x-2.5 items-center justify-start">
+                    <Button className='p-0 bg-transparent rounded-full' onClick={togglePlay}>
+                        <img src={isPlaying ? unmuteImage : muteImage} width={65} height={65} alt="restart" className='rounded-full' />
+                    </Button>
+                    {
+                        connection?.isConnected ? <Button className="p-0" onClick={disconnectWallet}>
+                            <div className="flex flex-row items-center">
+                                <div className="px-3.5 py-4 bg-[#272A32] rounded-tl-lg rounded-bl-lg">
+                                    <img src={connection.icon} className="w-6 h-6" />
+                                </div>
+                                <div className="px-3.5 py-5 bg-[#171922] rounded-tr-lg rounded-br-lg"><p className="text-white">{truncateString(address)}</p></div>
+                            </div>
+                        </Button> : <Button className='w-40 bg-[#F58229] rounded-md' onClick={connectWallet}>
+                            Connect Wallet
+                        </Button>
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,615 @@
+import eniola from '../assets/eniola.png'
+import israel from '../assets/israel.png'
+import energy from '../assets/energy.png'
+
+export const animate = {
+  mount: { scale: 1 },
+  unmount: { scale: 0.9 },
+}
+
+export const players = ['isreal', 'eniola']
+
+export const chat = [
+  {
+    user: 'isreal',
+    avatar: israel,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+  {
+    user: 'eniola',
+    avatar: eniola,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+  {
+    user: 'isreal',
+    avatar: israel,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+  {
+    user: 'eniola',
+    avatar: eniola,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+  {
+    user: 'isreal',
+    avatar: israel,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+  {
+    user: 'eniola',
+    avatar: eniola,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+  {
+    user: 'isreal',
+    avatar: israel,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+  {
+    user: 'eniola',
+    avatar: eniola,
+    message: "I will take it easy on you Eniola, but show me what you've got.",
+    date: '01:21pm',
+  },
+]
+
+export const initialSeeds = [
+  { pot: 1, seeds: 4 },
+  { pot: 2, seeds: 4 },
+  { pot: 3, seeds: 4 },
+  { pot: 4, seeds: 4 },
+  { pot: 5, seeds: 4 },
+  { pot: 6, seeds: 4 },
+  { pot: 7, seeds: 0 },
+  { pot: 8, seeds: 4 },
+  { pot: 9, seeds: 4 },
+  { pot: 10, seeds: 4 },
+  { pot: 11, seeds: 4 },
+  { pot: 12, seeds: 4 },
+  { pot: 13, seeds: 4 },
+  { pot: 14, seeds: 0 },
+]
+
+export const table_head = ['Player', 'Level', 'Score']
+
+export const stats = [
+  {
+    id: 1,
+    name: 'Energy',
+    image: energy,
+    level: 'Level 6',
+    score: 500000,
+  },
+  {
+    id: 2,
+    name: 'Eniola',
+    image: eniola,
+    level: 'Level 6',
+    score: 450000,
+  },
+  {
+    id: 3,
+    name: 'Israel',
+    image: israel,
+    level: 'Level 6',
+    score: 300000,
+  },
+  {
+    id: 4,
+    name: 'Energy',
+    image: energy,
+    level: 'Level 6',
+    score: 250000,
+  },
+  {
+    id: 5,
+    name: 'Eniola',
+    image: eniola,
+    level: 'Level 6',
+    score: 200000,
+  },
+  {
+    id: 6,
+    name: 'Israel',
+    image: israel,
+    level: 'Level 6',
+    score: 150000,
+  },
+  {
+    id: 7,
+    name: 'Energy',
+    image: energy,
+    level: 'Level 6',
+    score: 100000,
+  },
+  {
+    id: 8,
+    name: 'Eniola',
+    image: eniola,
+    level: 'Level 6',
+    score: 50000,
+  },
+]
+
+export const player_header = [
+  {
+    id: 1,
+    name: 'Player',
+  },
+  {
+    id: 2,
+    name: 'Level',
+  },
+  {
+    id: 3,
+    name: 'Score',
+  },
+  {
+    id: 4,
+    name: 'Duels',
+  },
+  {
+    id: 5,
+    name: 'Wins',
+  },
+  {
+    id: 6,
+    name: 'Losses',
+  },
+  {
+    id: 7,
+    name: 'Draws',
+  },
+]
+
+export const player_stats = [
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 1,
+    image: israel,
+    name: 'Israel',
+    level: 'Level 5',
+    score: '5982.10',
+    duels: 16,
+    wins: 12,
+    losses: 3,
+    draws: 1,
+  },
+  {
+    id: 2,
+    image: eniola,
+    name: 'Eniola',
+    level: 'Level 3',
+    score: '3982.10',
+    duels: 12,
+    wins: 8,
+    losses: 3,
+    draws: 1,
+  },
+]
+
+export const duels_header = [
+  {
+    id: 1,
+    name: 'Challenger',
+  },
+  {
+    id: 2,
+    name: 'Challenged',
+  },
+  {
+    id: 3,
+    name: 'Winner',
+  },
+  {
+    id: 4,
+    name: 'Date',
+  },
+  {
+    id: 5,
+    name: 'More',
+  },
+]
+
+export const duels_stats = [
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    winner: 'Eniola',
+    date: '14 - 05 - 2024',
+  },
+]
+
+export const live_duels_header = [
+  {
+    id: 1,
+    name: 'Challenger',
+  },
+  {
+    id: 2,
+    name: 'Challenged',
+  },
+  {
+    id: 3,
+    name: 'Date',
+  },
+  {
+    id: 4,
+    name: 'Status',
+  },
+]
+
+export const live_duels_stats = [
+  {
+    id: 0,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Join Game',
+  },
+  {
+    id: 1,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: null,
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Joined',
+  },
+  {
+    id: 2,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Join Game',
+  },
+  {
+    id: 3,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: null,
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Joined',
+  },
+  {
+    id: 4,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Join Game',
+  },
+  {
+    id: 5,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: null,
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Joined',
+  },
+  {
+    id: 6,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: {
+      name: 'Israel',
+      image: israel,
+    },
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Join Game',
+  },
+  {
+    id: 7,
+    challenger: {
+      name: 'Eniola',
+      image: eniola,
+    },
+    challenged: null,
+    date: '14 - 05 - 2024, 12:13PM',
+    status: 'Joined',
+  },
+]
