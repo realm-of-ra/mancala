0f619d2a786be7d855af9da67bbebeb4d7702522
supreme2580 <victoromorogbe69@gmail.com>
2024-06-22 13:33:09 +0100

    wip

--- client/src/components/players-lobby.tsx
+++ client/src/components/players-lobby.tsx
@@ -1,17 +1,63 @@
+import { eniola } from "@/constants/icons_store";
 import { player_header, player_stats } from "@/lib/constants";
+import { getPlayers, truncateString } from "@/lib/utils";
+import { useQuery, gql } from "@apollo/client";
 import { Card, Typography } from "@material-tailwind/react";
+import { useProvider } from "@starknet-react/core";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { constants } from "starknet";
+import { StarknetIdNavigator, StarkProfile } from "starknetid.js";
 
-export default function PlayersLobby() {
+export default function PlayersLobby({ data }: { data: any }) {
+
+    const { provider } = useProvider();
+
+    const starknetIdNavigator = new StarknetIdNavigator(
+        provider,
+        constants.StarknetChainId.SN_MAIN
+    );
+
+    // Extracting player_one and player_two from the data object
+    const players = getPlayers(data);
+
+    const addresses = players?.map((player: any) => player.address);
+
+    const [profiles, setProfiles] = useState<StarkProfile[]>([]);
+
+    useEffect(() => {
+        if (!starknetIdNavigator || !addresses) return;
+        (async () => {
+            const data = await starknetIdNavigator?.getStarkProfiles(addresses)
+            if (!data) return;
+            setProfiles(data);
+        })()
+    }, [addresses]);
+
+    if (!players) {
+        return <div className="w-[874px] h-[437px] flex flex-col items-center justify-center">
+            <svg className="text-white animate-spin w-fit" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"
+                width="24" height="24">
+                <path
+                    d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                    stroke="#F58229" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path
+                    d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                    stroke="#FCE3AA" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" className="text-gray-900">
+                </path>
+            </svg>
+        </div>
+    }
+
     return (
         <Card className="w-full h-full bg-transparent">
-            <table className="w-full text-left bg-transparent table-auto">
-                <thead className="border-b border-[#313640]">
+            <table className="text-left bg-transparent table-auto">
+                <thead className="sticky -top-3.5 bg-[#0F1116] z-10">
                     <tr>
-                        {player_header.map((head) => (
+                        {player_header.map((head, index) => (
                             <th
                                 key={head.id}
-                                className="p-4 w-[125px]"
+                                className={clsx("p-4", index === 0 ? "text-start" : "text-center")}
                             >
                                 <Typography
                                     variant="small"
@@ -22,90 +68,48 @@ export default function PlayersLobby() {
                             </th>
                         ))}
                     </tr>
-                </thead>
-                <tbody className="absolute h-[450px] w-[814px] overflow-y-scroll bg-[#16181D]">
-                    <table className="w-full text-left table-auto">
-                        <thead className="border-b border-[#313640] hidden">
-                            <tr>
-                                {player_header.map((head) => (
-                                    <th
-                                        key={head.id}
-                                        className="p-4"
-                                    >
-                                        <Typography
-                                            variant="small"
-                                            className="font-medium leading-none text-[#BDC2CC]"
-                                        >
-                                            {head.name}
-                                        </Typography>
-                                    </th>
-                                ))}
+                    <div className="w-full border-b border-[#313640] h-1 absolute inset-x-0 top-10" />
+                </thead >
+                <tbody>
+                    {players?.map(({ address, losses, wins, totalAppearances }: { address: string, losses: number, wins: number, totalAppearances: number }, index: number) => {
+                        const isLast = index === players.length - 1;
+                        return (
+                            <tr key={index} className={clsx(!isLast ? "border-b border-[#23272F]" : "", "bg-[#0F1116]")}>
+                                <td className="flex flex-row items-center p-4 space-x-5 max-w-fit">
+                                    <Typography>
+                                        <div className="flex flex-row items-center space-x-5 w-fit">
+                                            <img src={profiles ? profiles[index]?.profilePicture : ""} width={35} height={35} alt={`${profiles ? profiles[index]?.name : truncateString(address)} profile picture`} className="rounded-full" />
+                                            <p
+                                                className="font-normal text-white"
+                                            >
+                                                {profiles[index]?.name ? profiles[index]?.name : truncateString(address)}
+                                            </p>
+                                        </div>
+                                    </Typography>
+                                </td>
+                                <td>
+                                    <Typography className="font-normal text-[#FAB580] text-center">{wins < 4 ? 1 : Math.round(wins % 4)}</Typography>
+                                </td>
+                                <td>
+                                    <Typography className="font-normal text-[#FAB580] text-center">{wins * 50}</Typography>
+                                </td>
+                                <td>
+                                    <Typography className="font-normal text-[#FAB580] text-center">{totalAppearances}</Typography>
+                                </td>
+                                <td>
+                                    <Typography className="font-normal text-[#FAB580] text-center">{wins}</Typography>
+                                </td>
+                                <td>
+                                    <Typography className="font-normal text-[#FAB580] text-center">{losses}</Typography>
+                                </td>
+                                <td>
+                                    <Typography className="font-normal text-[#FAB580] text-center">0</Typography>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody className="w-full max-h-[450px] overflow-y-scroll">
-                            {player_stats.map(({ id, image, name, level, score, duels, wins, losses, draws }, index) => {
-                                const isLast = index === player_stats.length - 1;
-                                return (
-                                    <tr key={id} className={clsx(!isLast && "border-b border-[#23272F]", "w-full bg-[#0F1116]")}>
-                                        <td className="flex flex-row items-center w-full p-4 space-x-5">
-                                            <div className="flex flex-row items-center space-x-5 w-fit">
-                                                <img src={image} width={35} height={35} alt={`${name} profile picture`} className="rounded-full" />
-                                                <p
-                                                    className="font-normal text-white"
-                                                >
-                                                    {name}
-                                                </p>
-                                            </div>
-                                        </td>
-                                        <td className="w-[100px]">
-                                            <p
-                                                className="font-normal text-[#FAB580]"
-                                            >
-                                                {level}
-                                            </p>
-                                        </td>
-                                        <td className="w-[135px]">
-                                            <p
-                                                className="font-normal text-[#F97E22]"
-                                            >
-                                                {score.toLocaleString()}
-                                            </p>
-                                        </td>
-                                        <td className="w-[115px]">
-                                            <p
-                                                className="font-normal text-[#F97E22]"
-                                            >
-                                                {duels.toLocaleString()}
-                                            </p>
-                                        </td>
-                                        <td className="w-[125px]">
-                                            <p
-                                                className="font-normal text-[#F97E22]"
-                                            >
-                                                {wins.toLocaleString()}
-                                            </p>
-                                        </td>
-                                        <td className="w-[115px]">
-                                            <p
-                                                className="font-normal text-[#F97E22]"
-                                            >
-                                                {losses.toLocaleString()}
-                                            </p>
-                                        </td>
-                                        <td className="w-[90px]">
-                                            <p
-                                                className="font-normal text-[#F97E22]"
-                                            >
-                                                {draws.toLocaleString()}
-                                            </p>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
+                        );
+                    })}
                 </tbody>
-            </table>
-        </Card>
+            </table >
+        </Card >
     )
 }
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -15,3 +15,39 @@ export function truncateString(str?: string) {
 export function isEmptyString(str?: string): boolean {
   return !str || typeof str != "string" || str.trim() === "";
 }
+
+export function getPlayers(data: any) {
+  // Extracting player_one and player_two from the data object
+  const players = data?.reduce((acc: any, edge: any) => {
+    const { player_one, player_two, winner } = edge.node;
+
+    // Update player_one
+    const playerOneIndex = acc.findIndex((player: any) => player.address === player_one);
+    if (playerOneIndex !== -1) {
+        if (winner === player_one) {
+            acc[playerOneIndex].wins++;
+        } else {
+            acc[playerOneIndex].loses++;
+        }
+        acc[playerOneIndex].totalAppearances++;
+    } else {
+        acc.push({ address: player_one, wins: winner === player_one ? 1 : 0, losses: winner !== player_one ? 1 : 0, totalAppearances: 1 });
+    }
+
+    // Update player_two
+    const playerTwoIndex = acc.findIndex((player: any) => player.address === player_two);
+      if (playerTwoIndex !== -1) {
+          if (winner === player_two) {
+              acc[playerTwoIndex].wins++;
+          } else {
+              acc[playerTwoIndex].loses++;
+          }
+          acc[playerTwoIndex].totalAppearances++;
+      } else {
+          acc.push({ address: player_two, wins: winner === player_two ? 1 : 0, losses: winner !== player_two ? 1 : 0, totalAppearances: 1 });
+      }
+
+      return acc;
+  }, []);
+  return players;
+}
