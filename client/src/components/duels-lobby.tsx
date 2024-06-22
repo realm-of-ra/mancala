4cd9ec8cd99eb4b4b42819229b5a7d6851546c73
Emmaunuel Ejembi <ejembioche50@gmail.com>
2024-06-22 13:13:53 +0000

    implementation of logic for a user duels

--- client/src/components/duels-lobby.tsx
+++ client/src/components/duels-lobby.tsx
@@ -1,97 +1,146 @@
-import { duels_header, duels_stats, player_header, player_stats } from "@/lib/constants";
+import { duels_header, player_header, player_stats } from "@/lib/constants";
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
 import { Card, Typography } from "@material-tailwind/react";
+import { useProvider } from "@starknet-react/core";
 import clsx from "clsx";
+import { useEffect, useMemo, useState } from "react";
+import { StarknetIdNavigator } from "starknetid.js";
+import { constants, StarkProfile } from "starknet";
+import { truncateString } from "@/lib/utils";
+import { LoadingSkeleton } from "@/components/ui/loading-skeleton"
 
-export default function DuelsLobby() {
+export default function DuelsLobby({ games, transactions }: { games: any, transactions: any }) {
+    const { provider } = useProvider();
+    const starknetIdNavigator = useMemo(() => {
+        return new StarknetIdNavigator(
+            provider,
+            constants.StarknetChainId.SN_MAIN
+        );
+    }, [provider])
+    const [challengers, setChallengers] = useState<StarkProfile[]>();
+    const [challenged, setChallenged] = useState<StarkProfile[]>();
+    const [winners, setWinners] = useState<StarkProfile[]>();
+    const challengerAddresses = games.map((game: any) => game.node.player_one);
+    const challengedAddresses = games.map((game: any) => game.node.player_two);
+    const winnersAddresses = games.map((game: any) => game.node.winner);
+    console.log("lobby", games[0].node)
+    useEffect(() => {
+        if (!starknetIdNavigator || !challengerAddresses) return;
+        (async () => {
+            const challengerData = await starknetIdNavigator?.getStarkProfiles(challengerAddresses)
+            const challengedData = await starknetIdNavigator?.getStarkProfiles(challengedAddresses)
+            const winnersData = await starknetIdNavigator?.getStarkProfiles(winnersAddresses)
+            if (!challengerData) return;
+            if (challengerData) setChallengers(challengerData);
+            if (!challengedData) return;
+            if (challengedData) setChallenged(challengedData);
+            if (!winnersData) return;
+            if (winnersData) setWinners(winnersData)
+        })();
+    }, [challengerAddresses, challengedAddresses, winnersAddresses, starknetIdNavigator]);
+    const data = challengers?.map((challenger, index) => {
+        return {
+            challenger: challenger,
+            challenged: challenged ? challenged[index] : null,
+            winner: winners ? winners[index] : null,
+            date: transactions[index].node.executedAt,
+        }
+    })
+
     return (
-        <Card className="w-full h-full bg-transparent">
-            <table className="w-full text-left bg-transparent table-auto">
-                <thead className="border-b border-[#313640] flex flex-row items-center justify-between w-full">
-                    <tr className="flex flex-row items-center justify-between w-full">
-                        {duels_header.map((head) => (
-                            <th
-                                key={head.id}
-                                className="p-4"
-                            >
-                                <Typography
-                                    variant="small"
-                                    className="font-medium leading-none text-[#BDC2CC]"
+        <div className="w-[874px] h-[874px] bg-[url('./assets/lobby-box-long.png')] bg-contain bg-no-repeat p-8">
+            <Card className="w-full h-full bg-transparent">
+                <table className="w-full text-left bg-transparent table-auto">
+                    <thead className="border-b border-[#313640] flex flex-row items-center justify-between w-full">
+                        <tr className="flex flex-row items-center justify-between w-full">
+                            {duels_header.map((head) => (
+                                <th
+                                    key={head.id}
+                                    className="p-4"
                                 >
-                                    {head.name}
-                                </Typography>
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className="absolute h-[450px] w-[814px] overflow-y-scroll bg-[#16181D]">
-                    <table className="w-full text-left table-auto">
-                        <thead className="border-b border-[#313640] hidden">
-                            <tr className="w-full bg-[#0F1116] flex flex-row items-center justify-between">
-                                {player_header.map((head) => (
-                                    <th
-                                        key={head.id}
-                                        className="p-4"
+                                    <Typography
+                                        variant="small"
+                                        className="font-medium leading-none text-[#BDC2CC]"
                                     >
-                                        <Typography
-                                            variant="small"
-                                            className="font-medium leading-none text-[#BDC2CC]"
+                                        {head.name}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody className="absolute h-[450px] w-[814px] overflow-y-scroll bg-[#16181D]">
+                        <table className="w-full text-left table-auto">
+                            <thead className="border-b border-[#313640] hidden">
+                                <tr className="w-full bg-[#0F1116] flex flex-row items-center justify-between">
+                                    {player_header.map((head) => (
+                                        <th
+                                            key={head.id}
+                                            className="p-4"
                                         >
-                                            {head.name}
-                                        </Typography>
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody className="w-full max-h-[450px] overflow-y-scroll">
-                            {duels_stats.map(({ id, challenger, challenged, winner, date }, index) => {
-                                const isLast = index === player_stats.length - 1;
-                                return (
-                                    <tr key={id} className={clsx(!isLast && "border-b border-[#23272F]", "w-full bg-[#0F1116] flex flex-row items-center")}>
-                                        <td className="flex flex-row items-center p-4 space-x-5 w-[175px] justify-start">
-                                            <div className="flex flex-row items-center space-x-5 w-fit">
-                                                <img src={challenger.image} width={35} height={35} alt={`${name} profile picture`} className="rounded-full" />
-                                                <p
-                                                    className="font-normal text-white"
-                                                >
-                                                    {challenger.name}
-                                                </p>
-                                            </div>
-                                        </td>
-                                        <td className="flex flex-row items-center p-4 space-x-5 w-[175px] justify-center">
-                                            <div className="flex flex-row items-center space-x-5 w-fit">
-                                                <img src={challenged.image} width={35} height={35} alt={`${name} profile picture`} className="rounded-full" />
-                                                <p
-                                                    className="font-normal text-white"
-                                                >
-                                                    {challenged.name}
-                                                </p>
-                                            </div>
-                                        </td>
-                                        <td className="w-[175px] text-center">
-                                            <p
-                                                className="font-normal text-[#FAB580]"
+                                            <Typography
+                                                variant="small"
+                                                className="font-medium leading-none text-[#BDC2CC]"
                                             >
-                                                {winner}
-                                            </p>
-                                        </td>
-                                        <td className="w-[175px] text-center">
-                                            <p
-                                                className="font-normal text-[#F97E22]"
-                                            >
-                                                {date}
-                                            </p>
-                                        </td>
-                                        <td className="w-[175px] flex flex-row justify-center">
-                                            <EllipsisHorizontalIcon className="w-6 h-6 text-white" />
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </tbody>
-            </table >
-        </Card >
+                                                {head.name}
+                                            </Typography>
+                                        </th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody className="w-full max-h-[450px] overflow-y-scroll">
+                                {data ? data.map((data: any, index: number) => {
+                                    console.log("lobby", data);
+                                    const isLast = index === player_stats.length - 1;
+                                    const date = new Date(data.date)
+                                    return (
+                                        <tr key={index} className={clsx(!isLast && "border-b border-[#23272F]", "w-full bg-[#0F1116] flex flex-row items-center")}>
+                                            <td className="flex flex-row items-center p-4 space-x-5 w-[175px] justify-start">
+                                                <div className="flex flex-row items-center space-x-5 w-fit">
+                                                    <img src={data.challenger.profilePicture} width={35} height={35} alt={`${data.challenger.name} profile picture`} className="rounded-full" />
+                                                    <p
+                                                        className="font-normal text-white"
+                                                    >
+                                                        {data.challenger.name ? data.challenger.name : truncateString(games[index].node.player_one)}
+                                                    </p>
+                                                </div>
+                                            </td>
+                                            <td className="flex flex-row items-center p-4 space-x-5 w-[175px] justify-center">
+                                                {
+                                                    games[index].node.player_two !== "0x0" ? <div className="flex flex-row items-center space-x-2.5 w-fit">
+                                                        <img src={data.challenged.profilePicture} width={35} height={35} alt={`${data.challenged.name} profile picture`} className="rounded-full" />
+                                                        <p
+                                                            className="font-normal text-white"
+                                                        >
+                                                            {data.challenged.name ? data.challenged.name : truncateString((games[index].node.player_two))}
+                                                        </p>
+                                                    </div> : <p className="text-white">Matchmaking</p>
+                                                }
+                                            </td>
+                                            <td className="w-[175px] text-center">
+                                                <p
+                                                    className="font-normal text-[#FAB580]"
+                                                >
+                                                    {data.winner.name ? data.winner.name : truncateString((games[index].node.winner))}
+                                                </p>
+                                            </td>
+                                            <td className="w-[175px] text-center">
+                                                <p
+                                                    className="font-normal text-[#F97E22]"
+                                                >
+                                                    {date.toLocaleDateString()}
+                                                </p>
+                                            </td>
+                                            <td className="w-[175px] flex flex-row justify-center">
+                                                <EllipsisHorizontalIcon className="w-6 h-6 text-white" />
+                                            </td>
+                                        </tr>
+                                    );
+                                }) : <LoadingSkeleton />}
+                            </tbody>
+                        </table>
+                    </tbody>
+                </table >
+            </Card >
+        </div>
     )
 }
