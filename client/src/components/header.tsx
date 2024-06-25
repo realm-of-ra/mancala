0289138d8a21315065a8200146f4a841e6867d9c
Hunter001 <146340502+od-hunter@users.noreply.github.com>
2024-06-25 18:27:29 +0000

    feat: create disconnect/connect drop menu

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getPlayer, truncateString } from "../lib/utils";
 import mancala from "../assets/logo.png";
 import eniola from "../assets/eniola.png";
@@ -17,6 +17,14 @@ import { UserIcon } from "@heroicons/react/24/solid";
 import { StarkProfile } from "@/types";
 import { useQuery, gql } from "@apollo/client";
 import { useDojo } from "@/dojo/useDojo";
+import icon1 from './Logo - Golden 2.svg'
+import icon2 from './Logo - Golden 2.svg'
+import icon3 from './LogoW.svg'
+import iconArr from './logo-arrow.svg'
+import connectB from './connect.svg'
+import leader from './leader.svg'
+import profile from './profile.svg'
+import lobby from './lobby.svg'
 
 export default function Header() {
     const [connection, setConnection] = useAtom(connectionAtom);
@@ -100,6 +108,27 @@ export default function Header() {
 
     console.log(player)
 
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isDropdownClose, setIsDropdownClose] = useState(false);
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleConnect = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+    connectWallet()
+  };
+
+  const handleDropdownToggleClose = () => {
+    setIsDropdownClose(!isDropdownClose);
+  };
+  const handleDisconnect = () => {
+    setIsDropdownClose(!isDropdownClose);
+    disconnectWallet()
+  };
+
+
     return (
         <div className="flex flex-row items-center justify-between w-full">
             <div className="flex-1 w-full -mr-10">
@@ -147,18 +176,74 @@ export default function Header() {
                     <Button className='p-0 bg-transparent rounded-full' onClick={togglePlay}>
                         <img src={isPlaying ? unmuteImage : muteImage} width={65} height={65} alt="restart" className='rounded-full' />
                     </Button>
-                    {
-                        connection?.isConnected ? <Button className="p-0" onClick={disconnectWallet}>
-                            <div className="flex flex-row items-center">
-                                <div className="px-3.5 py-4 bg-[#272A32] rounded-tl-lg rounded-bl-lg">
-                                    <img src={connection.icon} className="w-6 h-6" />
-                                </div>
-                                <div className="px-3.5 py-5 bg-[#171922] rounded-tr-lg rounded-br-lg"><p className="text-white">{truncateString(address)}</p></div>
-                            </div>
-                        </Button> : <Button className='w-40 bg-[#F58229] rounded-md' onClick={connectWallet}>
-                            Connect Wallet
-                        </Button>
-                    }
+                    <div className="relative">
+    {connection?.isConnected ? (
+        <div className="relative">
+        <Button className="p-0 flex justify-between relative items-center bg-[#171922] w-[259px] text-sm white whitespace-nowrap rounded-full " >
+            <div className="flex w-[259px] flex-row items-center">
+                <div className="px-3.5 py-4 bg-[#272A32] rounded-tl-lg rounded-bl-lg">
+                    <img src={connection.icon} className="w-6 h-6" />
+                </div>
+                <div className="px-3.5 py-5 rounded-tr-lg rounded-br-lg">
+                    <p className="text-white">{truncateString(address)}</p>
+                </div>
+                <img src={iconArr} className={`ml-3 transition duration-300 ${isDropdownClose ? 'transform rotate-180' : ''}`} onClick={handleDropdownToggleClose} />
+            </div>
+        </Button>
+    
+        {isDropdownClose && (
+            <div className="absolute top-1/2 transform translate-y-4 w-[259px] right-0 mt-2 py-2 w-48 border border-gray-400 p-4 bg-[#171922] text-white rounded-md shadow-xl z-20">
+                <span className="flex">
+                    <img src={lobby} />
+                    <a href="#" className="block px-4 py-2 text-white">Lobby</a>
+                </span>
+                <span className="flex">
+                    <img src={leader} />
+                    <a href="#" className="block px-4 py-2 text-white">Leadership</a>
+                </span>
+                <span className="flex">
+                    <img src={profile} />
+                    <a href="#" className="block px-4 py-2 text-white">Profile</a>
+                </span>
+                <span className="flex">
+                    <img src={connectB} />
+                    <a href="#" className="block px-4 py-2 text-white whitespace-nowrap" onClick={handleDisconnect}>Disconnect Wallet</a>
+                </span>
+            </div>
+        )}
+    </div>
+    ) : (
+        <Button className='flex justify-between relative items-center bg-[#F58229] w-[259px] text-sm white whitespace-nowrap rounded-full' onClick={() => console.log()}>
+            <span className="bg-[#FFA158] h-full flex items-center rounded-tl-full rounded-bl-full absolute left-0 top-0 bottom-0 px-4">
+                <img src={icon3} className="text-left" />
+            </span> 
+            <span className="ml-12">Connect Wallet</span>
+            <img src={iconArr} className={`ml-3 transition duration-300 ${isDropdownOpen ? 'transform rotate-180' : ''}`} onClick={handleDropdownToggle} />
+        </Button>
+    )}
+
+    {isDropdownOpen && (
+        <div className="absolute top-1/2 transform translate-y-4 w-[259px] right-0 mt-2 py-2 w-48 border border-gray-400 p-4 bg-[#171922] text-white rounded-md shadow-xl z-20">
+            <span className="flex">
+                <img src={lobby} />
+                <a href="#" className="block px-4 py-2 text-white">Lobby</a>
+            </span>
+            <span className="flex">
+                <img src={leader} />
+                <a href="#" className="block px-4 py-2 text-white">Leadership</a>
+            </span>
+            <span className="flex">
+                <img src={profile} />
+                <a href="#" className="block px-4 py-2 text-white">Profile</a>
+            </span>
+            <span className="flex">
+                <img src={connectB} />
+                <a href="#" className="block px-4 py-2 text-white whitespace-nowrap" onClick={handleConnect}>connect Wallet</a>
+            </span>
+        </div>
+    )}
+</div>
+                    
                 </div>
             </div>
         </div>
