b088cb2d8285bf6c95dad2549611d5b8f206793d
okhai <57156589+okhaimie-dev@users.noreply.github.com>
2024-06-25 22:23:57 -0500

    feat: wip dojojs upgrade (#124)
    * feat: wip dojojs upgrade
    
    * fix: burnerManager err
    
    * fix: burnerManager fix
    
    * fix: build issue / reverted starknet version back to previous

--- client/src/dojo/generated/setup.ts
+++ client/src/dojo/generated/setup.ts
@@ -1,39 +1,39 @@
-import { getSyncEntities } from '@dojoengine/state'
-import { DojoConfig, DojoProvider } from '@dojoengine/core'
-import * as torii from '@dojoengine/torii-client'
-import { createClientComponents } from '../createClientComponents'
-import { createSystemCalls } from '../createSystemCalls'
-import { defineContractComponents } from './contractComponents'
-import { world } from './world'
-import { setupWorld } from './generated'
-import { Account, ProviderOptions } from 'starknet'
-import { BurnerManager } from '@dojoengine/create-burner'
+import { getSyncEntities } from "@dojoengine/state";
+import { DojoConfig, DojoProvider } from "@dojoengine/core";
+import * as torii from "@dojoengine/torii-client";
+import { createClientComponents } from "../createClientComponents";
+import { createSystemCalls } from "../createSystemCalls";
+import { defineContractComponents } from "./contractComponents";
+import { world } from "./world";
+import { setupWorld } from "./generated";
+import { Account, WeierstrassSignatureType } from "starknet";
+import { BurnerManager } from "@dojoengine/create-burner";
 
-export type SetupResult = Awaited<ReturnType<typeof setup>>
+export type SetupResult = Awaited<ReturnType<typeof setup>>;
 
 export async function setup({ ...config }: DojoConfig) {
   // torii client
   const toriiClient = await torii.createClient([], {
     rpcUrl: config.rpcUrl,
     toriiUrl: config.toriiUrl,
-    relayUrl: '',
-    worldAddress: config.manifest.world.address || '',
-  })
+    relayUrl: "",
+    worldAddress: config.manifest.world.address || "",
+  });
 
   // create contract components
-  const contractComponents = defineContractComponents(world)
+  const contractComponents = defineContractComponents(world);
 
   // create client components
-  const clientComponents = createClientComponents({ contractComponents })
+  const clientComponents = createClientComponents({ contractComponents });
 
   // fetch all existing entities from torii
-  await getSyncEntities(toriiClient, contractComponents as any)
+  await getSyncEntities(toriiClient, contractComponents as any, []);
 
   // create dojo provider
-  const dojoProvider = new DojoProvider(config.manifest, config.rpcUrl)
+  const dojoProvider = new DojoProvider(config.manifest, config.rpcUrl);
 
   // setup world
-  const client = await setupWorld(dojoProvider)
+  const client = await setupWorld(dojoProvider);
 
   // create burner manager
   const burnerManager = new BurnerManager({
@@ -47,15 +47,15 @@ export async function setup({ ...config }: DojoConfig) {
     accountClassHash: config.accountClassHash,
     rpcProvider: dojoProvider.provider,
     feeTokenAddress: config.feeTokenAddress,
-  })
+  });
 
   try {
-    await burnerManager.init()
+    await burnerManager.init();
     if (burnerManager.list().length === 0) {
-      await burnerManager.create()
+      await burnerManager.create();
     }
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 
   return {
@@ -67,8 +67,14 @@ export async function setup({ ...config }: DojoConfig) {
       contractComponents,
       clientComponents,
     ),
+    publish: (typedData: string, signature: WeierstrassSignatureType) => {
+      toriiClient.publishMessage(typedData, {
+        r: signature.r.toString(),
+        s: signature.s.toString(),
+      });
+    },
     config,
     dojoProvider,
     burnerManager,
-  }
+  };
 }
