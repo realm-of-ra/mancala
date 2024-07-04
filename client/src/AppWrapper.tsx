956e21fec402715fd714d1069ae722ed45352806
xpanvictor <xpanvictor@gmail.com>
2024-07-04 09:44:29 +0100

    feat: error boundary added and also moves connection part of app to AppWrapper
    resolves #139

---
+++ client/src/AppWrapper.tsx
@@ -0,0 +1,48 @@
+import App from "./App.tsx";
+import "./index.css";
+import { setup } from "./dojo/generated/setup.ts";
+import { DojoProvider } from "./dojo/DojoContext.tsx";
+import { dojoConfig } from "../dojoConfig.ts";
+import { ApolloProvider } from '@apollo/client';
+import appoloClient from "./lib/appolo-client.ts";
+import { useEffect, useState } from "react";
+import CreateLoaderSVG from "./components/ui/svgs/create-loader.tsx";
+
+export default function AppWrapper() {
+  const [setupResult, setSetupResult] = useState<any>(null)
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error|null>(null);
+
+  useEffect(() => {
+    setup(dojoConfig)
+      .then(res => setSetupResult(res))
+      .catch(errorConnectingDojo => {
+        console.error("Connection error", errorConnectingDojo)
+        setError(new Error(errorConnectingDojo))
+        // throw errorConnectingDojo
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (loading) return (
+    <div className="w-full h-screen grid place-items-center bg-primary">
+      <div
+        className="flex flex-row items-center justify-center space-x-1">
+        <CreateLoaderSVG />
+        <p className="text-[#FCE3AA] font-semibold">Loading...</p>
+      </div>
+    </div>
+  )
+
+  if (error) throw error;
+
+  return (
+    <ApolloProvider client={appoloClient}>
+      <DojoProvider value={setupResult}>
+        <App />
+      </DojoProvider>
+    </ApolloProvider>
+
+  )
+}
+
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,33 +5,27 @@ import "./index.css";
 import { setup } from "./dojo/generated/setup.ts";
 import { DojoProvider } from "./dojo/DojoContext.tsx";
 import { dojoConfig } from "../dojoConfig.ts";
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+import ErrorBoundary from "./components/boundaries/error-boundary.tsx";
+import appoloClient from "./lib/appolo-client.ts";
+import AppWrapper from "./AppWrapper.tsx";
 
 async function init() {
-    const rootElement = document.getElementById("root");
-    if (!rootElement) throw new Error("React root not found");
-    const root = ReactDOM.createRoot(rootElement as HTMLElement);
+  const rootElement = document.getElementById("root");
+  if (!rootElement) throw new Error("React root not found");
+  const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-    const setupResult = await setup(dojoConfig);
+  const Loading = () => <div className="text-4xl text-black">Loading...</div>;
 
-    const client = new ApolloClient({
-        uri: 'https://api.cartridge.gg/x/mancala/torii/graphql',
-        cache: new InMemoryCache(),
-    })
-
-    const Loading = () => <div className="text-4xl text-black">Loading...</div>;
-
-    root.render(
-        <React.StrictMode>
-            <ApolloProvider client={client}>
-                <Suspense fallback={<Loading />}>
-                    <DojoProvider value={setupResult}>
-                        <App />
-                    </DojoProvider>
-                </Suspense>
-            </ApolloProvider>
-        </React.StrictMode>
-    );
+  root.render(
+    <React.StrictMode>
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <AppWrapper />
+        </Suspense>
+      </ErrorBoundary>
+    </React.StrictMode>
+  );
 }
 
 init();
