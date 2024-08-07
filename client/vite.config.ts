f7891d9decebb686ac6d32d1e94065a70126d91a
Tony Stark <oktonystark8@gmail.com>
2024-08-07 14:32:16 -0400

    mkcert add

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import wasm from 'vite-plugin-wasm'
-import react from '@vitejs/plugin-react'
-import topLevelAwait from 'vite-plugin-top-level-await'
-import path from 'path'
+import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import react from "@vitejs/plugin-react";
+import topLevelAwait from "vite-plugin-top-level-await";
+import path from "path";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [react(), wasm(), topLevelAwait(), mkcert()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
@@ -18,7 +19,5 @@ export default defineConfig({
       external: [],
     },
   },
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg'], // Add this line to ensure assets are included
-
-})
-
+  assetsInclude: ["**/*.png", "**/*.jpg", "**/*.svg"], // Add this line to ensure assets are included
+});
