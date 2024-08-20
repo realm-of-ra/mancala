b06767ce997aff5e823b4caae3a85505102162b3
Tony Stark <oktonystark8@gmail.com>
2024-08-20 11:17:59 -0400

    feat: passing on to supreme-dev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Leaderboard from "./pages/Leaderboard";
 import { Provider as JotaiProvider } from "jotai";
 import { sepolia, Chain } from "@starknet-react/chains";
-import { StarknetConfig, jsonRpcProvider } from "@starknet-react/core";
+import {
+  StarknetConfig,
+  jsonRpcProvider,
+  Connector,
+} from "@starknet-react/core";
 import Gameplay from "./pages/games/Gameplay";
 import Home from "./pages/Home";
 import Lobby from "./pages/Lobby";
 import { useEffect, useState } from "react";
 import CartridgeConnector from "@cartridge/connector";
-import { useDojo } from "./dojo/useDojo";
+import { ControllerOptions } from "@cartridge/controller";
 
 function rpc(_chain: Chain) {
   return {
@@ -30,66 +34,63 @@ export default function App() {
     };
   }, []);
 
-  const { burner } = useDojo();
-
-  const connector = new CartridgeConnector(
-    [
-      {
-        target: burner.feeTokenAddress,
-        method: "approve",
-      },
-      {
-        target:
-          "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
-        method: "create_initial_game_id",
-      },
-      {
-        target:
-          "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
-        method: "create_game",
-      },
-      {
-        target:
-          "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
-        method: "create_private_game",
-      },
-      {
-        target:
-          "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
-        method: "join_game",
-      },
-      {
-        target:
-          "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
-        method: "move",
-      },
-      {
-        target:
-          "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
-        method: "time_out",
-      },
-    ],
+  const policies = [
     {
-      rpc: "https://api.cartridge.gg/x/mancala-alpha-v3/katana",
-      theme: "realm-of-ra",
+      target:
+        "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
+      method: "create_initial_game_id",
     },
-  );
+    {
+      target:
+        "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
+      method: "create_game",
+    },
+    {
+      target:
+        "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
+      method: "create_private_game",
+    },
+    {
+      target:
+        "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
+      method: "join_game",
+    },
+    {
+      target:
+        "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
+      method: "move",
+    },
+    {
+      target:
+        "0x4c4869d1067625074edd5f2c68774831ee26261423b8a17fc71f533e7b03f5c",
+      method: "time_out",
+    },
+  ];
+
+  const options: ControllerOptions = {
+    rpc: "https://api.cartridge.gg/x/mancala-alpha-v3/katana",
+    theme: "realm-of-ra",
+  };
+
+  const connectors = [
+    new CartridgeConnector(policies, options) as never as Connector,
+  ];
 
   return (
     <StarknetConfig
       chains={[sepolia]}
       provider={jsonRpcProvider({ rpc })}
-      connectors={[connector]}
+      connectors={connectors}
     >
       <JotaiProvider>
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route index element={<Home />} />
             <Route path="/lobby" element={<Lobby />} />
             <Route path="/leaderboard" element={<Leaderboard />} />
             <Route path="/games/:gameId" element={<Gameplay />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </JotaiProvider>
       {isSmallScreen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center text-white bg-black bg-opacity-75 backdrop-blur-sm">
