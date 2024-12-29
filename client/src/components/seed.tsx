bc68b64b9b9cbf158fb4929394115f3c6a8bd7d8
Supreme2580 <victoromorogbe69@gmail.com>
2024-12-29 20:11:52 +0100

    wip

--- client/src/components/seed.tsx
+++ client/src/components/seed.tsx
@@ -28,7 +28,7 @@ export default function Seed({
         4: { x: -355, y: 120 },
         5: { x: -235, y: 120 },
         6: { x: -115, y: 120 },
-        7: { x: -55, y: 83 },
+        7: { x: (length || 0) > 24 ? -40 : -15, y: 90 },
       }
     : {
         1: { x: 90, y: 120 },
@@ -37,7 +37,7 @@ export default function Seed({
         4: { x: 445, y: 120 },
         5: { x: 565, y: 120 },
         6: { x: 685, y: 120 },
-        7: { x: 775, y: 83 },
+        7: { x: (length || 0) > 24 ? 1000 : 1040, y: 90 },
       };
 
   const opponentPositions = isNative
@@ -48,7 +48,7 @@ export default function Seed({
         4: { x: 325, y: 10 },
         5: { x: 205, y: 10 },
         6: { x: 85, y: 10 },
-        7: { x: 50, y: 83 },
+        7: { x: (length || 0) > 24 ? 40 : 12, y: 90 },
       }
     : {
         1: { x: -110, y: 10 },
@@ -57,7 +57,7 @@ export default function Seed({
         4: { x: -475, y: 10 },
         5: { x: -595, y: 10 },
         6: { x: -715, y: 10 },
-        7: { x: -725, y: 83 },
+        7: { x: (length || 0) > 24 ? -945 : -1025, y: 90 },
       };
 
   // Updated grid position calculation
