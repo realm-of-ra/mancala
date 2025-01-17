54259e5c8e3dbc5b942401c8226a32e7b2c3e21a
Supreme2580 <victoromorogbe69@gmail.com>
2025-01-18 00:31:40 +0100

    thumb border removed

--- client/src/components/ui/slider.tsx
+++ client/src/components/ui/slider.tsx
@@ -16,7 +16,7 @@ const Slider = React.forwardRef<any, any>(({ className, track, thumb, range, hid
     <SliderPrimitive.Track className={clsx("relative h-2 w-full grow overflow-hidden rounded-full bg-[#C7CAD4]", track)}>
       <SliderPrimitive.Range className={clsx("absolute h-full bg-white", range)} />
     </SliderPrimitive.Track>
-    {!hideThumb && <SliderPrimitive.Thumb className={clsx("block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50", thumb)} />}
+    {!hideThumb && <SliderPrimitive.Thumb className={clsx("block h-5 w-5 rounded-full bg-background transition-colors focus-visible:outline-none border-0 disabled:pointer-events-none disabled:opacity-50", thumb)} />}
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
