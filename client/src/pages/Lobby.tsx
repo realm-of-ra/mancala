a5cb85065d7aba7987e2578227992702ece242a8
Supreme2580 <victoromorogbe69@gmail.com>
2025-01-18 00:41:22 +0100

    added survey button below

--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -513,6 +513,9 @@ export default function Lobby() {
           </div>
         </div>
       </div>
+      <Link to="/" className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
+          <Button className="bg-[#0F1116] hover:bg-[#0F1116] text-[#C7CAD4] font-medium hover:cursor-pointer rounded-xl">Give feedbacks and get a chance to win Lord of the Mancala</Button>
+      </Link>
     </div>
   );
 }
