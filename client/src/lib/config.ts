6fcc82c4e3c20ec57ee6f23d3ee0c051239b1c38
Supreme2580 <victoromorogbe69@gmail.com>
2025-02-17 19:55:49 +0100

    implemented

--- client/src/lib/config.ts
+++ client/src/lib/config.ts
@@ -1,17 +1,17 @@
 export const IS_MAINNET = import.meta.env.VITE_BLOCKCHAIN_TYPE === "mainnet"
-const MANCALA_ADDRESS = IS_MAINNET ? "" : "0x5920638dd022083587d85417b642e85a16dc08bfe9ce8a536e17462cb332fd3"
-const RPC_URL = IS_MAINNET ? "" : "https://api.cartridge.gg/x/starknet/sepolia"
-const TORII_URL = IS_MAINNET ? "" : "https://api.cartridge.gg/x/mancala-b/torii"
+const MANCALA_ADDRESS = IS_MAINNET ? import.meta.env.MAINNET_MANCALA_ADDRESS : import.meta.env.SEPOLIA_MANCALA_ADDRESS
+const RPC_URL = IS_MAINNET ? import.meta.env.MAINNET_RPC_URL : import.meta.env.SEPOLIA_RPC_URL
+const TORII_URL = IS_MAINNET ? import.meta.env.MAINNET_TORII_URL : import.meta.env.SEPOLIA_TORII_URL
 
 const CONFIG = {
   GRAPHQL_ENDPOINT: `${TORII_URL}/graphql`,
   SLOT_RPC_URL: RPC_URL,
   RPC_URL,
   TORII_URL,
-  MASTER_ADDRESS: IS_MAINNET ? "" : "0x02A54eFf65D55a2e7D2fC7E23eF0F0e6030dB0313b16c8F0eaD0b4a83807f6d6",
-  MASTER_PRIVATE_KEY: IS_MAINNET ? "" : "0x0396533b7a1182ef03da483eca82e9dd2b17bee9c7fec07ac6cbbeb8bfd573cb",
+  MASTER_ADDRESS: IS_MAINNET ? import.meta.env.MAINNET_MASTER_ADDRESS : import.meta.env.SEPOLIA_MASTER_ADDRESS,
+  MASTER_PRIVATE_KEY: IS_MAINNET ? import.meta.env.MAINNET_MASTER_PRIVATE_KEY : import.meta.env.SEPOLIA_MASTER_PRIVATE_KEY,
   MANCALA_ADDRESS,
-  PROFILE_ADDRESS: IS_MAINNET ? "" : "0x3714f8b523e83a41c722a8c6f21da9b6d9aee178bc7ed3daeeb2d4649ebf4bd",
+  PROFILE_ADDRESS: IS_MAINNET ? import.meta.env.MAINNET_PROFILE_ADDRESS : import.meta.env.SEPOLIA_PROFILE_ADDRESS,
   POLICIES: [
     {
       target: MANCALA_ADDRESS,
